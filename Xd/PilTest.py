2bd2d2d8fde360e32859f713247309b4600aafd0
Fernanman <97917922+Fernanman@users.noreply.github.com>
2023-08-11 15:59:42 -0500

    Delete Xd directory

--- Xd/PilTest.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image, ImageColor
-
-im = Image.new('1', (1,1)) # create the Image of size 1 pixel 
-im.putpixel((0,0), ImageColor.getcolor('black', '1')) # or whatever color you wish
-
-im.save('simplePixel.png') # or any image format
